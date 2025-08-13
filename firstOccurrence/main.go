3c79669bc2500de7adba385e002e662acff0fe51
Your Name <charlesterhuysen303@gmail.com>
2025-08-13 09:02:52 +0200

    Updated for loop to <=

--- firstOccurrence/main.go
+++ firstOccurrence/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	haystack := "leetcode"
-	needle := "cod"
+	haystack := "abc"
+	needle := "c"
 
 	fmt.Println(strStr(haystack, needle))
 }
@@ -15,6 +15,9 @@ func strStr(haystack string, needle string) int {
 	}
 	if len(needle) > len(haystack) {
 		return -1
+	}
+	if (len(haystack) == 1 && len(needle) == 1) && haystack == needle {
+		return 0
 	}
 
 	for i := 0; i < len(haystack)-len(needle); i++ {
