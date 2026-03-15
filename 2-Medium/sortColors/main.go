c8678781bf58339cb4f8a642048890fde2ca41f1
Ch-E <charlesterhuysen303@gmail.com>
2026-03-15 17:12:34 -0500

    Removed refresher folder

--- 2-Medium/sortColors/main.go
+++ 2-Medium/sortColors/main.go
@@ -10,7 +10,7 @@ func main() {
 }
 
 // Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects
-// of the same color are adjacent, with the colors in the order red, white, and blue..
+// of the same color are adjacent, with the colors in the order red, white, and blue...
 func sortColors(nums []int) {
 	colorCount := []int{0, 0, 0}
 
