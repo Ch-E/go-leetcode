e8c4d09337939add5d73688acb2d17d613bf964c
Ch-E <charlesterhuysen303@gmail.com>
2026-03-17 06:38:54 -0500

    kidsWithCandies - added optimized solution O(n)

--- 1-Easy/kidsWithCandies/main.go
+++ 1-Easy/kidsWithCandies/main.go
@@ -40,3 +40,26 @@ func kidsWithCandies(candies []int, extraCandies int) []bool {
 
 	return result
 }
+
+/*
+Optimized solution O(n):
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	max := 0
+
+	for i := 0; i < len(candies); i++ {
+		if max < candies[i] {
+			max = candies[i]
+		}
+	}
+
+	result := make([]bool, len(candies))
+
+	for i := 0; i < len(candies); i++ {
+		if candies[i]+extraCandies >= max {
+			result[i] = true
+		}
+	}
+
+	return result
+}
+*/
