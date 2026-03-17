cb2c58d7a85bf156a868f62684a9d04983c7e45f
Ch-E <charlesterhuysen303@gmail.com>
2026-03-17 06:30:10 -0500

    kidsWithCandies

---
+++ 1-Easy/kidsWithCandies/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	candies := []int{2, 3, 5, 1, 3}
+	extraCandies := 3
+
+	fmt.Println(kidsWithCandies(candies, extraCandies))
+}
+
+/*
+There are n kids with candies. You are given an integer array candies,
+where each candies[i] represents the number of candies the ith kid has,
+and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if,
+after giving the ith kid all the extraCandies, they will have the greatest
+number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+*/
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	result := make([]bool, 0)
+
+	for i := 0; i < len(candies); i++ {
+		isGreatest := true
+		sumCandies := candies[i] + extraCandies
+
+		for j := 0; j < len(candies); j++ {
+			if sumCandies < candies[j] {
+				isGreatest = false
+				break
+			}
+		}
+
+		result = append(result, isGreatest)
+	}
+
+	return result
+}
