65b858aeb000e747fb3b16206a3c0f4fba39e1b9
Ch-E <charlesterhuysen303@gmail.com>
2026-03-21 10:19:25 -0500

    increasingTriplet

---
+++ 2-Medium/increasingTrplet/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(increasingTriplet(nums))
+}
+
+/*
+Given an integer array nums, return true if there exists a triple of indices (i, j, k)
+such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+*/
+func increasingTriplet(nums []int) bool {
+
+}
