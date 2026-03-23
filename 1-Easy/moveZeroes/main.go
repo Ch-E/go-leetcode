047692081c173c24f92611a243412f6820bf7bd6
Ch-E <charlesterhuysen303@gmail.com>
2026-03-23 06:55:33 -0500

    moveZeroes move pointer from right side

--- 1-Easy/moveZeroes/main.go
+++ 1-Easy/moveZeroes/main.go
@@ -27,3 +27,15 @@ func moveZeroes(nums []int) {
 		}
 	}
 }
+
+/*
+func moveZeroes(nums []int)  {
+    left := 0
+    for right := 0; right < len(nums); right++ {
+        if nums[right] != 0 {
+            nums[left], nums[right] = nums[right], nums[left]
+            left++
+        }
+    }
+}
+*/
