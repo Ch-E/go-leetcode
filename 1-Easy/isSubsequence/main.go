91bf5e33d1f1a3fbbbe7d0edc28e082494e789f3
Ch-E <charlesterhuysen303@gmail.com>
2026-03-23 20:26:33 -0500

    isSubsequence clean implementation

--- 1-Easy/isSubsequence/main.go
+++ 1-Easy/isSubsequence/main.go
@@ -37,3 +37,16 @@ func isSubsequence(s string, t string) bool {
 
 	return matchingCount == len(s)
 }
+
+/*
+func isSubsequence(s string, t string) bool {
+    i, j := 0, 0
+    for i < len(s) && j < len(t) {
+        if s[i] == t[j] {
+            i++
+        }
+        j++
+    }
+    return i == len(s)
+}
+*/
