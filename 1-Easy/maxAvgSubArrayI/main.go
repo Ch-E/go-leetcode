c982648a3f5de979787b0d742ecd0d320a393fdd
Ch-E <charlesterhuysen303@gmail.com>
2026-03-25 22:24:35 -0500

    maxAvgSubArrayI

---
+++ 1-Easy/maxAvgSubArrayI/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 12, -5, -6, 50, 3}
+	k := 4
+
+	fmt.Println(findMaxAverage(nums, k))
+}
+
+/*
+You are given an integer array nums consisting of n elements, and an integer k.
+Find a contiguous subarray whose length is equal to k that has the maximum average value
+and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+*/
+func findMaxAverage(nums []int, k int) float64 {
+
+}
