74a31065e208df356343daea8b17844dcfb0e40e
Ch-E <charlesterhuysen303@gmail.com>
2026-03-26 07:07:22 -0500

    maxAvgSubArrayI

--- 1-Easy/maxAvgSubArrayI/main.go
+++ 1-Easy/maxAvgSubArrayI/main.go
@@ -12,8 +12,14 @@ func main() {
 /*
 You are given an integer array nums consisting of n elements, and an integer k.
 Find a contiguous subarray whose length is equal to k that has the maximum average value
-and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+and return this value. Any answer with a calculation error less than 10^-5 will be accepted.
 */
 func findMaxAverage(nums []int, k int) float64 {
+	j := 1
 
+	for i := 0; i < len(nums); i++ {
+		for j < k && j < len(nums) {
+
+		}
+	}
 }
