0ee1dc658001f84bc2dc25be58ffe8f827a7f523
Ch-E <charlesterhuysen303@gmail.com>
2026-03-26 21:44:22 -0500

    maxAvgSubArrayI

--- 1-Easy/maxAvgSubArrayI/main.go
+++ 1-Easy/maxAvgSubArrayI/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	nums := []int{1, 12, -5, -6, 50, 3}
@@ -15,11 +18,19 @@ Find a contiguous subarray whose length is equal to k that has the maximum avera
 and return this value. Any answer with a calculation error less than 10^-5 will be accepted.
 */
 func findMaxAverage(nums []int, k int) float64 {
-	j := 1
+	result := make([]float64, 0)
 
-	for i := 0; i < len(nums); i++ {
-		for j < k && j < len(nums) {
-
+	for i := 0; i <= len(nums)-k; i++ {
+		j := i + 1
+		sum := nums[i]
+		for j < i+k {
+			sum += nums[j]
+			j++
 		}
+
+		avg := float64(sum) / float64(k)
+		result = append(result, float64(avg))
 	}
+
+	return slices.Max(result)
 }
