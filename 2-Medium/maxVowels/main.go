de0c4e3888b7fc11fdcc353842363f6ed4257904
Ch-E <charlesterhuysen303@gmail.com>
2026-03-27 06:38:02 -0500

    maxVowels

---
+++ 2-Medium/maxVowels/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "abciiidef"
+	k := 3
+
+	fmt.Println(maxVowels(s, k))
+}
+
+/*
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+*/
+func maxVowels(s string, k int) int {
+
+}
