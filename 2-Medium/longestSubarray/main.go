c6dc630cc693ba9c7aee61458fa21fe5087d2ef8
Ch-E <charlesterhuysen303@gmail.com>
2026-03-28 21:17:43 -0500

    longestSubarray test

--- 2-Medium/longestSubarray/main.go
+++ 2-Medium/longestSubarray/main.go
@@ -10,5 +10,22 @@ Return the size of the longest non-empty subarray containing only 1's in the res
 Return 0 if there is no such subarray.
 */
 func longestSubarray(nums []int) int {
+	left, right := 0, 0
+	k := 1
 
+	for right < len(nums) {
+		if nums[right] == 0 {
+			k--
+		}
+		right++
+
+		if k < 0 {
+			if nums[left] == 0 {
+				k++
+			}
+			left++
+		}
+	}
+
+	return right - left
 }
