88e858eaeec12c916c7de46375f8dadd92d008d9
Ch-E <charlesterhuysen303@gmail.com>
2026-04-05 07:09:00 -0500

    findDifference Optimized - idiomatic Set in Go

--- 1-Easy/findDifference/main.go
+++ 1-Easy/findDifference/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
 func main() {
@@ -19,37 +18,30 @@ answer[1] is a list of all distinct integers in nums2 which are not present in n
 Note that the integers in the lists may be returned in any order.
 */
 func findDifference(nums1 []int, nums2 []int) [][]int {
-	uniqueNums1 := make([]int, 0)
-	uniqueNums2 := make([]int, 0)
+	seen1 := make(map[int]bool)
+	seen2 := make(map[int]bool)
+	unique1 := make([]int, 0)
+	unique2 := make([]int, 0)
 
-	for i := 0; i < len(nums1); i++ {
-		isMatch := false
-		for j := 0; j < len(nums2); j++ {
-			if nums1[i] == nums2[j] {
-				isMatch = true
-			}
-		}
+	for _, v := range nums1 {
+		seen1[v] = true
+	}
 
-		if !isMatch {
-			uniqueNums1 = append(uniqueNums1, nums1[i])
+	for _, v := range nums2 {
+		seen2[v] = true
+	}
+
+	for i := range seen1 {
+		if !seen2[i] {
+			unique1 = append(unique1, i)
 		}
 	}
 
-	for i := 0; i < len(nums2); i++ {
-		isMatch := false
-		for j := 0; j < len(nums1); j++ {
-			if nums2[i] == nums1[j] {
-				isMatch = true
-			}
-		}
-
-		if !isMatch {
-			uniqueNums2 = append(uniqueNums2, nums2[i])
+	for i := range seen2 {
+		if !seen1[i] {
+			unique2 = append(unique2, i)
 		}
 	}
 
-	uniqueNums1 = slices.Compact(uniqueNums1)
-	uniqueNums2 = slices.Compact(uniqueNums2)
-
-	return [][]int{uniqueNums1, uniqueNums2}
+	return [][]int{unique1, unique2}
 }
